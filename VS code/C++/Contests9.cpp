c06706a9f410af53b8652de26156717eba63b10c
ThienPhu-K61 <82692148+ThienPhu-coder@users.noreply.github.com>
2021-09-21 18:46:21 -0700

    Add files via upload

---
+++ VS code/C++/Contests9.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****   A. 101010   *****
+/*int main() {
+    int T, a;
+    cin >> T;
+    while(T--) {
+        cin >> a;
+        if(a % 10 == 0) {
+            cout << "YES" << endl;
+        } else cout << "NO" << endl;
+    }
+    system("pause");
+    return 0;
+}*/
+
+// *****   B. 12134321   *****
+/*int main() {
+    int a[4] = {121, 12321, 1234321, 123454321};
+    int T, N;
+    cin >> T;
+    while(T--) {
+        cin >> N;
+        for(int i = 0; i < 4; i++) {
+            if(N % a[i] == 0) {
+                cout << "YES" << endl;
+                break;
+            }
+            else if(i == 3) {
+                cout << "NO" << endl;
+            }
+        }
+    }
+    system("pause");
+    return 0;
+}*/
+
+// *****   C. Dấu ngoặc   *****
+int main() {
+    int T, 
+    system("pause");
+    return 0;
+}
